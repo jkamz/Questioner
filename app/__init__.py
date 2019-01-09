dc36df16316286762b28ad93559ef881e160c380
jkamz <jkamz6432@gmail.com>
2019-01-09 16:52:22 +0300

    [finishes#163070006] setup app initialization

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+"""Creating the app"""
+
+import os
+from flask import Flask
+from instance.config import app_config
+
+
+def create_app(config_name):
+    '''create app using correct configurations'''
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.secret_key = os.getenv("SECRET")
+
+    return app
