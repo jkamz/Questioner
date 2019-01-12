63f2136b92b4a79c9c2b95c8d80cf1f36fe5f686
jkamz <jkamz6432@gmail.com>
2019-01-12 09:07:25 +0300

    [finishes#163164979] finish creating questions tests

--- app/tests/v1/test_questions.py
+++ app/tests/v1/test_questions.py
@@ -19,19 +19,34 @@ class QuestionTest(unittest.TestCase):
         self.question = {
             "author": "jkamz",
             "body": "what did you say?",
-            "created_on": "07:38am Saturday 12 January 2019",
-            "meetup_id": 2,
-            "question_id": 3,
-            "title": "what",
-            "votes": 0
+            "meetup_id": 1,
+            "title": "what"
+        }
+
+        self.meetup = {
+            "host": "jkamz",
+            "location": "Nairobi",
+            "occuring_on": "28th Jan",
+            "summary": "Getting to know python",
+            "tags": "python pythonista flask",
+            "topic": "python"
         }
 
         self.question1 = {}
 
+        self.vote = {
+            "questionId": 1
+        }
+
     def test_create_question(self):
         '''test the endpoint of creating new question record'''
 
-        res = self.client.post("api/v1/questions", data=json.dumps(self.question),
+        # post meetup
+        res = self.client.post("api/v1/create_meetup", data=json.dumps(self.meetup),
+                               content_type="application/json")
+
+        # post question
+        res = self.client.post("api/v1/meetups/1/questions", data=json.dumps(self.question),
                                content_type="application/json")
 
         response_data = json.loads(res.data.decode())
@@ -42,12 +57,53 @@ class QuestionTest(unittest.TestCase):
     def test_create_invalid_question(self):
         '''test the endpoint of creating a new question record invalidly'''
 
-        res = self.client.post("api/v1/questions", data=json.dumps(self.question),
+        res = self.client.post("api/v1/meetups/1/questions", data=json.dumps(self.question1),
                                content_type="application/json")
 
         response_data = json.loads(res.data.decode())
-
         self.assertEqual(res.status_code, 400)
-        self.assertFalse(response_data["data"])
+        self.assertIn("expects only Application/JSON data", str(response_data))
 
     def test_upvote_question(self):
+        '''test the endpoint of upvoting a question'''
+
+        # upvote question
+        res = self.client.patch("api/v1/questions/1/upvote", data=json.dumps(self.vote),
+                                content_type="application/json")
+
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("upvote successfull", str(response_data))
+
+    def test_invalid_upvote_question(self):
+        '''test the endpoint of upvoting an inexistent question'''
+
+        # upvote question
+        res = self.client.patch("api/v1/questions/456/upvote", data=json.dumps(self.vote),
+                                content_type="application/json")
+
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("upvote not successful", str(response_data))
+
+    def test_downvote_question(self):
+        '''test the endpoint of downvoting a question'''
+
+        # downvote question
+        res = self.client.patch("api/v1/questions/1/downvote", data=json.dumps(self.vote),
+                                content_type="application/json")
+
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("downvote successfull", str(response_data))
+
+    def test_invalid_downvote_question(self):
+        '''test the endpoint of downvoting an inexistent question'''
+
+        # downvote question
+        res = self.client.patch("api/v1/questions/456/downvote", data=json.dumps(self.vote),
+                                content_type="application/json")
+
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("downvote not successful", str(response_data))
