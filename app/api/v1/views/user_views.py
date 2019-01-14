ae748ac42e8c1f3410f3ca3f35f2b657a551a4e4
jkamz <jkamz6432@gmail.com>
2019-01-14 21:09:40 +0300

    [feature#163071015]add user signin and signup views

---
+++ app/api/v1/views/user_views.py
@@ -0,0 +1,47 @@
+"""
+Create views for all user endpoints
+"""
+from flask import request, Blueprint, jsonify
+
+from ..models import user_models
+
+userbp = Blueprint('userbp', __name__, url_prefix='/api/v1')
+
+
+@userbp.route('/signup', methods=['POST'])
+def sign_up():
+    '''endpoint for adding a user
+    '''
+    user_data = request.get_json()
+    if user_data:
+        firstname = user_data.get('firstname')
+        lastname = user_data.get('lastname')
+        othername = user_data.get('othername')
+        isAdmin = user_data.get('isAdmin')
+        email = user_data.get('email')
+        username = user_data.get('username')
+        password = user_data.get('password')
+        phoneNumber = user_data.get('phoneNumber')
+
+        new_user = user_models.User().signUp(firstname, lastname, othername, username, email, password, isAdmin, phoneNumber)
+
+        return jsonify({"status": 201, "data": new_user}), 201
+
+    return jsonify({"status": 400, "message": "expects only Application/JSON data"}), 400
+
+
+@userbp.route('/signin', methods=['POST'])
+def sign_in():
+    '''endpoint for adding a user
+    '''
+    user_data = request.get_json()
+
+    if user_data:
+        username = user_data.get('username')
+        password = user_data.get('password')
+        isAdmin = user_data.get('isAdmin')
+
+        user = user_models.User().signIn(username, password, isAdmin)
+        return jsonify({"status": 200, "data": user}), 200
+
+    return jsonify({"status": 400, "message": "expects only Application/JSON data"}), 400
