adb51e7fc052215afeae2235ccf4fb59b879cf27
jkamz <jkamz6432@gmail.com>
2019-01-15 20:24:46 +0300

    [starts#163222276] add validations to check email and usernames

---
+++ app/api/v1/utils/validators.py
@@ -0,0 +1,35 @@
+import re
+from ..models.user_models import users
+"""
+Add base validations
+"""
+
+
+class Validators():
+    """Validations for user input"""
+
+    def validate_email(self, email):
+        '''match provided email using regex'''
+
+        regx = "^[\w]+[\d]?@[\w]+\.[\w]+$"
+        return re.match(regx, email)
+
+    def validate_unique_email(self, email):
+        '''check if email already exists'''
+
+        user = [user for user in users if user["email"] == email]
+
+        if user:
+            return True
+
+        return False
+
+    def validate_unique_username(self, username):
+        '''check if username already exists'''
+
+        user = [user for user in users if user["username"] == username]
+
+        if user:
+            return True
+
+        return False
