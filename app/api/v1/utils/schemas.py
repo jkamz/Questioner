d2232fe406154a563afbc7338bff2da5ec46d83d
jkamz <jkamz6432@gmail.com>
2019-01-16 20:04:47 +0300

    [chore#163222276] create validation for max input length

--- app/api/v1/utils/schemas.py
+++ app/api/v1/utils/schemas.py
@@ -2,13 +2,14 @@ from marshmallow import Schema, fields, validate
 
 
 not_blank = validate.Length(min=1, error="Field cannot be blank")
+max_length = validate.Length(max=20)
 
 
 class UsersSchema(Schema):
 
-    firstname = fields.String(required=True, validate=not_blank)
-    lastname = fields.String(required=True, validate=not_blank)
-    username = fields.String(required=True, validate=not_blank)
+    firstname = fields.String(required=True, validate=(not_blank, max_length))
+    lastname = fields.String(required=True, validate=(not_blank, max_length))
+    username = fields.String(required=True, validate=(not_blank, max_length))
     email = fields.Email(required=True)
     password = fields.String(required=True)
     isAdmin = fields.Boolean()
@@ -16,6 +17,6 @@ class UsersSchema(Schema):
 
 class MeetingsSchema(Schema):
 
-    topic = fields.String(required=True, validate=not_blank)
-    location = fields.String(required=True, validate=not_blank)
+    topic = fields.String(required=True, validate=(not_blank, max_length))
+    location = fields.String(required=True, validate=(not_blank, max_length))
     happeningOn = fields.DateTime("%Y-%m-%d %H:%M")
