4cfe9dc5d8128109034a9597451ff1b6d1d99d4a
jkamz <jkamz6432@gmail.com>
2019-01-22 13:06:03 +0300

    [chore#163373532] add print function for table creation

--- app/database_connect.py
+++ app/database_connect.py
@@ -75,8 +75,7 @@ def create_tables():
         for table in tables:
             curr.execute(table)
         conn.commit()
-
-        return conn
+        print("tables created")
 
     except (Exception, psycopg2.DatabaseError) as error:
         raise error
