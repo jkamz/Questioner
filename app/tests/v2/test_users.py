2272a82c79d0e0ab8d9ac072c4729ff86b8addd1
jkamz <jkamz6432@gmail.com>
2019-01-22 21:06:00 +0300

    [chore#163373532] rename user tests file

--- app/tests/v2/test_users.py
+++ app/tests/v2/test_users.py
@@ -1,32 +1,36 @@
 """
-Tests for questions operations
+Tests for users operations
 """
 
 import unittest
 import json
 
-from app.database_connect import connect, destroy_database, create_tables
+from app.database_connect import destroy_database, create_tables, connect
 
 from ... import create_app
 
 
-class UserTest(unittest.TestCase):
+class TestUser(unittest.TestCase):
     """class representing the users test case"""
 
     def setUp(self):
         '''initialize the app and define test variable'''
-        self.app = create_app(config_name="testing")
-        self.client = self.app.test_client()
-        self.app_context = self.app
 
+        config_name = "testing"
+
+        self.app = create_app(config_name)
+        self.client = self.app.test_client()
+        #self.app_context = self.app
+
+        connect()
         create_tables()
 
         self.user = {
             "firstname": "andela",
             "lastname": "bootcamp",
-            "email": "andelapac@pac.com",
-            "username": "andech",
+            "email": "andeelapac@pac.com",
             "phoneNumber": "+254705107566",
+            "username": "andeche",
             "password": "Jkamz6432@"
         }
 
@@ -44,6 +48,10 @@ class UserTest(unittest.TestCase):
 
     def test_user_sign_up(self):
         '''test the endpoint for signing up a new user'''
+
+        connect()
+        create_tables()
+
         res = self.client.post("api/v2/auth/signup", data=json.dumps(self.user),
                                content_type="application/json")
 
@@ -52,5 +60,15 @@ class UserTest(unittest.TestCase):
         self.assertTrue(response_data["data"])
         self.assertIn("User created successfully", str(response_data))
 
+    def test_invalid_user_sign_up(self):
+        '''test the endpoint for signing up a new user invalidly'''
+        res = self.client.post("/api/v2/auth/signup", data=json.dumps(self.user1),
+                               content_type="application/json")
+
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertIn("expects only Application/JSON data", str(response_data))
+
     def tearDown(self):
         ''' Purge all posted data before running tests again '''
+        destroy_database()
