bf6f6266d97e2e8009104b603da90676a15d9722
jkamz <jkamz6432@gmail.com>
2019-01-22 13:05:08 +0300

    [chore#163373532] update create tables function in init

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,14 @@ import os
 from flask import Flask
 from instance.config import app_config
 
-from database_connect import create_tables
+from app import database_connect
 from .api.v2.views.user_views import auth
 
 
 def create_app(config_name):
     '''create app using correct configurations'''
 
-    create_tables()
+    database_connect.create_tables()
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
