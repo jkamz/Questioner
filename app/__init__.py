0161c24ce81034944946a0f6b7147c1432ac3578
jkamz <jkamz6432@gmail.com>
2019-01-22 12:19:29 +0300

    [chore#163373532] add create tables function to app init

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,20 @@ import os
 from flask import Flask
 from instance.config import app_config
 
+from database_connect import create_tables
+from .api.v2.views.user_views import auth
+
 
 def create_app(config_name):
     '''create app using correct configurations'''
+
+    create_tables()
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.secret_key = os.getenv("SECRET")
+
+    app.register_blueprint(auth)
 
     return app
