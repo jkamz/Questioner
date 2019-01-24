bb417f31ca0677b7912b57b23bb5d2c5161b9909
jkamz <jkamz6432@gmail.com>
2019-01-24 11:28:22 +0300

    [ft#163446512] add create question view

---
+++ app/api/v2/views/question_views.py
@@ -0,0 +1,36 @@
+"""
+Create views for all questions endpoints
+"""
+from flask import request, Blueprint, jsonify, make_response
+
+from ..models import question_models
+from ..utils.schemas import QuestionsSchema
+
+schema = QuestionsSchema()
+
+questionbp = Blueprint('questionbp', __name__, url_prefix='/api/v2')
+
+
+@questionbp.route('meetups/<int:meetup_id>/questions', methods=["POST"])
+def create_question(meetup_id):
+    '''
+    endpoint for creating a question record
+    '''
+
+    question_data = request.get_json()
+
+    if not question_data:
+        return jsonify({"status": 400, "message": "expects only Application/JSON data"}), 400
+
+    title = question_data.get('title')
+    body = question_data.get('body')
+    author = question_data.get('author')
+
+    data, errors = schema.load(question_data)
+    if errors:
+        return make_response(jsonify({"status": 400, "errors": errors})), 400
+
+    new_questionObj = question_models.Questions(meetup_id, title, body, author)
+    new_question = new_questionObj.createQuestion()
+
+    return jsonify({"status": 201, "data": new_question}), 201
